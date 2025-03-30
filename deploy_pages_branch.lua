e5e023001b00f976c4f5546adae6c6f9b86a4322
mbartlett21 <mjmouse9999@gmail.com>
2025-03-31 09:23:04 +1000

    Add deploy script

---
+++ deploy_pages_branch.lua
@@ -0,0 +1,102 @@
+-- first we read the script data from the files
+
+local used_files = {
+   'index.html',
+   'main.css',
+   'scripts/Main.js',
+   'scripts/theme.js',
+}
+
+local current_branch
+do
+   local p <close> = io.popen'git branch --show-current'
+   current_branch = p:read()
+end
+
+-- the workflow is:
+-- 1. read data from the files
+-- 2. replace javascript and css names with timestamped ones for caching (they can possibly be left if they are the same as previous)
+-- 3. Replace references in index.html
+-- 4. `git checkout pages`
+-- 5. Replace the files there.
+-- 6. commit and push and then checkout master again
+
+local ts = os.time()
+
+local datename = os.date('%Y-%m-%d %H:%M:%S', ts)
+
+local file_to_contents = {}
+
+for _, v in ipairs(used_files) do
+   local f <close> = io.open(v, 'r')
+   file_to_contents[v] = f:read'a'
+end
+
+local file_to_newfn = {}
+
+for k in pairs(file_to_contents) do
+   local s, ext = k:match'^(.*)(%.%w+)$'
+   if ext == '.js' or ext == '.css' then
+      -- we need a timestamp
+      local newfn = s .. '.' .. ts .. ext
+      file_to_newfn[k] = newfn
+   end
+end
+
+-- we now map the contents of any html files with references
+
+local function toplainpattern(str)
+   return str:gsub('%W', '%%%0')
+end
+
+for k, v in pairs(file_to_contents) do
+   if k:match'%.html?$' then
+      for inp, outp in pairs(file_to_newfn) do
+         v = v:gsub(toplainpattern(inp), outp)
+      end
+      file_to_contents[k] = v
+   end
+end
+
+-- we now do our git execution
+
+os.execute'git checkout pages'
+
+-- delete files apart from .git
+
+for item in io.popen'dir /S /A-D /B':lines() do
+   if item:match'%.git' then
+      goto next
+   end
+
+   local ext = item:match'%.%w+$'
+
+   if ext == '.js' or ext == '.css' or ext == '.html' then
+      print('removing ' .. item)
+      os.remove(item)
+   end
+
+   ::next::
+end
+
+-- add in our files
+
+for k, v in pairs(file_to_contents) do
+   k = file_to_newfn[k] or k
+
+   print('writing ' .. k)
+   local f <close> = io.open(k, 'wb')
+   f:write(v)
+end
+
+-- add them to git
+
+os.execute'git add *'
+
+-- commit
+
+os.execute('git commit -m "Deploy ' .. datename .. '"')
+
+os.execute('git push')
+
+os.execute('git checkout ' .. current_branch)
